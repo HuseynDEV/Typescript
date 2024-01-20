24c6a2f43187d91c944a905dfbe0abf858eb8be6
Huseyn Alizade <huseyna@maestro.st>
2024-01-20 23:03:52 +0400

    added new codes

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log('data types');
+//---------------------Arrays--------------------------------
+var firstNames1 = ["Can", "Huseyn", "Ahmet"];
+var firstNames2 = ["Can", "Huseyn", "Ahmet"];
+console.log(firstNames2);
+// 
+var arr = [1, 2, 'Name', true, false, "Paul"];
+// 
+var fruits;
+fruits = ["adam", "john"];
+var ids;
+ids = [1, 2, 5, 6];
+var values1 = ["Apple", 12, 23, "MacBook"];
+var values2 = ["Apple", 12, 23, "MacBook"];
+console.log(values1, values2);
+// --------------------------------------Tuple-------------------------------------------
+var Id = 1;
+var instructorName = "Huseyn";
+// deyerleri uygun olaraq vermek lazimdir yeni 1e number 2e string
+var instructor = [1, "Huseyn"];
+console.log(instructor);
+var objects;
+objects = {
+    age: 20,
+    name: "Huseyn",
+    worker: true
+};
+var person;
+person = {
+    age: 20,
+    name: "Huseyn",
+    worker: true
+};
+console.log(objects.age);
+// -----------------------------------------Union------------------------------------
+// enum Media {
+//     Newspaper=1,
+//     Newsletter,
+//     Magazine,
+// }
+// enum PrintMedia {
+//     Newspaper="NEWS",
+//     Newsletter="Letter",
+//     Magazine="Magazine",
+// }
+// console.log(PrintMedia.Newsletter)
--- app.ts
+++ app.ts
@@ -1,1 +1,76 @@
-console.log('data types')
+//---------------------Arrays--------------------------------
+let firstNames1: string[] = ["Can", "Huseyn", "Ahmet"]
+let firstNames2: Array<string> = ["Can", "Huseyn", "Ahmet"]
+console.log(firstNames2)
+// 
+let arr = [1, 2, 'Name', true, false, "Paul"]
+// 
+let fruits: Array<string>;
+fruits = ["adam", "john"]
+
+
+
+let ids: Array<number>;
+ids = [1, 2, 5, 6]
+
+
+let values1: (string | number)[] = ["Apple", 12, 23, "MacBook"]
+
+let values2: Array<string | number> = ["Apple", 12, 23, "MacBook"]
+
+console.log(values1, values2)
+
+
+// --------------------------------------Tuple-------------------------------------------
+
+let Id: number = 1
+let instructorName: string = "Huseyn"
+
+// deyerleri uygun olaraq vermek lazimdir yeni 1e number 2e string
+let instructor: [number, string] = [1, "Huseyn"]
+console.log(instructor)
+
+
+// --------------------------Object--------------------------------------------------
+
+type Person = {
+    age: number,
+    name: string,
+    worker: boolean
+}
+
+let objects: Person
+
+objects = {
+    age: 20,
+    name: "Huseyn",
+    worker: true
+}
+
+let person:{
+    age: number,
+    name: string,
+    worker: boolean
+}
+person={
+    age: 20,
+    name: "Huseyn",
+    worker: true
+}
+console.log(objects.age)
+
+// -----------------------------------------Union------------------------------------
+// enum Media {
+//     Newspaper=1,
+//     Newsletter,
+//     Magazine,
+
+// }
+
+// enum PrintMedia {
+//     Newspaper="NEWS",
+//     Newsletter="Letter",
+//     Magazine="Magazine",
+// }
+
+// console.log(PrintMedia.Newsletter)
